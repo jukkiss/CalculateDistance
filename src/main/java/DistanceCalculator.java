07ba6cfde13bcc8507332da3644374bea7f0f2a6
Jukka <savela.j@gmail.com>
2024-10-10 15:09:11 +0300

    third commit

--- src/main/java/DistanceCalculator.java
+++ src/main/java/DistanceCalculator.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class DistanceCalculator {
+
+    public static double calculateDistance(double x1, double y1, double x2, double y2) {
+        double dx = x2 - x1;
+        double dy = y2 - y1;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
 }
